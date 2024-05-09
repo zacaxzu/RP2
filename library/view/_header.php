0a55716590b9f5c81b4a6061d65999424c36f55c
zacaxzu <74599453+zacaxzu@users.noreply.github.com>
2024-05-09 23:25:03 +0200

    Navigation bar added

--- library/view/_header.php
+++ library/view/_header.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf8">
-	<title>KnjižnicaApp</title>
-	<link rel="stylesheet" href="css/style.css">
+	<title>Druga zadaća</title>
+	<link rel="stylesheet" href="/view/_header.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-	<h1>Bla bla</h1>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Balance</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Overview</a></li>
+				<li><a href="#">Expenses</a></li>
+				<li><a href="#">New expense</a></li>
+				<li><a href="#">Settle up!</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a>Hello <?php echo htmlspecialchars($_POST['username']); ?>! </a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+			</ul>
+		</div>
+	</nav>
